c4f966ded888a9222a757dc34f761c2aaa71de1a
jeff <jeffpdecker@gmail.com>
2012-06-24 22:33:25 +0000

    check in of \truck\Sandbox\SDCardHellowWorld, a simple project that creates a text file on the SD card and writes a line of text to it.
    must add references:
    System.IO
    SecretLabs.NETMF.Hardware.Netduino;
    SecretLabs.NETMF.IO;

---
+++ Sandbox/SDCardHellowWorld/SDCardHellowWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using SecretLabs.NETMF.IO;
+
+namespace SDCardHellowWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                StorageDevice.MountSD("SD", SPI.SPI_module.SPI1, Pins.GPIO_PIN_D10);
+
+                FileStream filestream = new FileStream(@"\SD\DeckerKicksNetduinoAss.txt", FileMode.Create);
+                StreamWriter streamWriter = new StreamWriter(filestream);
+                streamWriter.WriteLine("SD card loggin, Muthafucko!");
+                streamWriter.Close();
+                filestream.Close();
+                StorageDevice.Unmount("SD");
+                Debug.Print("all done!");
+            }
+
+            catch (Exception ex)
+            {
+
+                Debug.Print(ex.ToString());
+            }
+
+        }
+
+    }
+}
