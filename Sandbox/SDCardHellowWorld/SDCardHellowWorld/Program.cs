b5c57106b583535f1406e96fdc1bdfed52eea765
jeff <jeffpdecker@gmail.com>
2012-07-20 18:07:12 +0000

    check in for SDCardHelloWorld.
    changed where the stopwatch started in the code.  previously the stopwatch started before mounting the SD card and starting a filestream and a steamwriter; thus the average for-loop times were burdened with this overhead. this rev moves the stopwatch start and stop immediately before and immediately after the for-loop.
    
    the per 1000-line times are noted in the comments of the code (e.g. "2500ms/1k-line") for various data types.  looks as though there's some advantage to writing binary (hex) data to the card, the the convince of writing ascii data probably outweighs the performance gain.  DeckerKicksNetduinoAss.txt has been updated. the single column shows elapsed milliseconds using this rev of code.
    
    the so-what: we can expect SD card logging to take between 1 to 3ms per entry.

--- Sandbox/SDCardHellowWorld/SDCardHellowWorld/Program.cs
+++ Sandbox/SDCardHellowWorld/SDCardHellowWorld/Program.cs
@@ -12,12 +12,12 @@ namespace SDCardHellowWorld
 {
     public class Program
     {
+        
 
         public static void Main()
         {
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-
+           
+            
             
             try
             {
@@ -25,25 +25,35 @@ namespace SDCardHellowWorld
 
                 FileStream filestream = new FileStream(@"\SD\DeckerKicksNetduinoAss.txt", FileMode.Append);
                 StreamWriter streamWriter = new StreamWriter(filestream);
+                long longToWrite = 12345678;
+                int intToWrite = 1234567;
+                byte[] byteArraryToWrite = { 0x68, 0x65, 0x79, 0x20, 0x66, 0x75, 0x63, 0x6b, 0x20, 0x79, 0x6f, 0x75, 0x2c, 0x20, 0x79, 0x6f, 0x75, 0x20, 0x66, 0x75, 0x63, 0x6b, 0x69, 0x6e, 0x67, 0x20, 0x66, 0x75, 0x63, 0x6b, 0x21 };
+                byte byteToWrite =  0x68 ;
 
+                Stopwatch stopwatch = Stopwatch.StartNew();  //baseline with no writes in the for-loop to the SD card is 240ms.
 
-                for (int i = 0; i < 1000; i++)
+                for (short i = 0; i < 1000; i++)
                 {
+                    //streamWriter.WriteLine(intToWrite);     // int =  8878ms/1k-line
+                    //streamWriter.WriteLine(longToWrite);  // long  = 9753ms/1k-line, 9982ms/1k-line
+                    //streamWriter.WriteLine("string data");  // string "some data" = ~1407/1k-line;
+                    //streamWriter.Write(i);
+                    streamWriter.WriteLine("\t" + stopwatch.ElapsedMilliseconds.ToString()); // this combined line is 2519ms/1k-line, 2274ms/1k-line
+                    //filestream.Write(byteArraryToWrite, 0, byteArraryToWrite.Length); //byte[] = 1776ms/1k-line
+                    //filestream.WriteByte(byteToWrite);  //byte = 804ms/1k-line
 
-                    streamWriter.Write("Here's a line muthafucko: ");
-                    streamWriter.Write(i);
-                    streamWriter.WriteLine("\t" + stopwatch.ElapsedMilliseconds.ToString());
 
-                    if (i % 100 == 0)
-                    {
-                        Debug.Print(i.ToString());
-                    }
+                    //if (i % 100 == 0)
+                    //{
+                    //    Debug.Print(i.ToString());
+                    //}
                 }
-
+                Debug.Print(stopwatch.ElapsedMilliseconds.ToString());
+                Debug.Print("================all done!======================");
                 streamWriter.Close();
                 filestream.Close();
                 StorageDevice.Unmount("SD");
-                Debug.Print("================all done!======================");
+
 
 
             }
